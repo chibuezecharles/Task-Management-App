bf506df2132dcc5ad1f423d1de10c1fcd76f4ce3
Chibueze Charles <charleschibuezec24@gmail.com>
2025-01-17 16:47:03 +0100

    update

--- src/api/json-server/index.js
+++ src/api/json-server/index.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
 const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '../../db.json')); // Correct path to db.json
+
+const router = jsonServer.router(path.join(__dirname, '../../../db.json')); 
+
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
